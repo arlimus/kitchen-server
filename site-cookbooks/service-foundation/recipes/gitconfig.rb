5b10b12060a5aa255b87fe68b8d31e3ef9100c2b
Dominik Richter <dominik.richter@gmail.com>
2013-10-25 10:57:51 +0200

    updated to chef-users cookbook and updated chef-zsh cookbook
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- site-cookbooks/service-foundation/recipes/gitconfig.rb
+++ site-cookbooks/service-foundation/recipes/gitconfig.rb
@@ -1,32 +1,23 @@
-# Install and configure zsh
-# * oh-my-zsh
-# * zshrc
-# * per-user configuration, depending on databags
+# Add ~/.gitconfig
 
-def get_userinfo c
-  [
-    c['id'],
-    c['gid'] || 'users',
-    c['home'] || File.join('home',uid)
-  ]
-end
-
-search("users","gitconfig:*").each do |c|
-  Chef::Log.info "gitconfig: setting git config for user #{c['id']}"
+# per-user config
+active_users.
+  find_all{|u| u['gitconfig'] == true }.
+  each do |c|
 
   # presets
-  uid, gid, home = get_userinfo c
+  u = get_userinfo c
 
   # package requirements
   package "git-core"
 
   # install the git configuration
-  gc_home = File.join home, '.gitconfig'
+  gc_home = File.join u[:home], '.gitconfig'
   template gc_home do
     source "gitconfig.erb"
     mode 0644
-    owner uid
-    group gid
+    owner u[:uid]
+    group u[:gid]
     variables({
        :name  => c['comment'],
        :email => c['email']
