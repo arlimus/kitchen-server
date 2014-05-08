8c99796c51a97115721c9342c57d224395cb60eb
Dominik Richter <dominik.richter@gmail.com>
2014-05-08 23:16:27 +0200

    separated the installation of puppet chef and vagrant
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- site-cookbooks/service-foundation/attributes/default.rb
+++ site-cookbooks/service-foundation/attributes/default.rb
@@ -1,3 +1,6 @@
 # set default attributes
 default['oh-my-zsh']['themes']  = [ ]
 default['oh-my-zsh']['plugins'] = [ ]
+default['install_chef'] = false
+default['install_puppet'] = false
+default['install_vagrant'] = false
--- site-cookbooks/service-foundation/recipes/default.rb
+++ site-cookbooks/service-foundation/recipes/default.rb
@@ -13,17 +13,33 @@ package "ruby1.9.1-full"
 execute "update-alternatives --set ruby /usr/bin/ruby1.9.1"
 execute "update-alternatives --set gem /usr/bin/gem1.9.1"
 
-# install system-wide gems
-%w{
-  librarian-puppet
-  puppet
-  berkshelf
-  vagrant-berkshelf
-  chef
-}.each{|g| gem_package g}
-
 # install extra core packages
 %w{
   htop
   curl
+  build-essential
+  vim
 }.each{|p| package p}
+
+if node['install_chef']
+  %w{
+    berkshelf
+    chef
+    foodcritic
+  }.each{|g| gem_package g}
+end
+
+if node['install_puppet']
+  %w{
+    librarian-puppet
+    puppet
+    puppet-lint
+  }.each{|g| gem_package g}
+end
+
+if node['install_vagrant']
+  %w{
+    kitchen-vagrant
+    vagrant-berkshelf
+  }.each{|g| gem_package g}
+end
