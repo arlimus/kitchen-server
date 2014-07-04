29da311e57f5ef9e486406d1d4cfbcf96d1f2edd
Dominik Richter <dominik.richter@gmail.com>
2014-07-04 19:14:30 +0200

    add hardening to default
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- roles/base.rb
+++ roles/base.rb
@@ -1,7 +1,15 @@
 name "base"
 description "Basic work server configuration"
+override_attributes({
+  'network' => {
+    'forwarding' => true
+  }
+})
+
 run_list %w{
   recipe[chef-solo-search]
+  recipe[os-hardening]
+  recipe[ssh-hardening]
   recipe[git]
   recipe[user::data_bag]
   recipe[user-unlock]
